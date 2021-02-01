123c3e7a218fe75fa7cd2e0ff33248cbaa5d9fe4
Uvin Weththasinghe <uvin6667@gmail.com>
2021-02-01 15:46:44 +0530

    Add customized exception classes

---
+++ src/main/java/lk/ijse/dep/web/lms/exception/HttpResponseException.java
@@ -0,0 +1,18 @@
+package lk.ijse.dep.web.lms.exception;
+
+public class HttpResponseException extends RuntimeException{
+    private int statusCode;
+
+    public HttpResponseException(int statusCode, String message, Throwable cause) {
+        super(message, cause);
+        this.setStatusCode(statusCode);
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
